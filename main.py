f65218ca5dfef03b7a109dc9722b31d336437856
skillfulsuhas <spsuhas2004@gmail.com>
2023-01-22 17:14:23 +0530

    gui changes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
 from tkinter import *
 window=Tk()
-window.title("Weather app")
-window.config(background="black")
-window.geometry("260x580")
+window.title("Weather App ")
+window.config(background="light blue")
+window.geometry("700x580")
 
 city = StringVar()
-
 api_city = ""
 country = StringVar()
 temp = StringVar()
@@ -49,28 +48,23 @@ def get_weather_search():
     
 
 search_city=StringVar()
-Label(window,text="WEATHER APP",bg="black",fg="white",font="none 30 bold").grid(row=0,column=0)
-Label(window, text=" ",bg="black",fg="white",font="none 15 bold" ).grid(row=1,column=0)
+Label(window,text="WEATHER APP",bg="white",fg="black",font="Verdana 30 bold").grid(row=0,column=0)
+Label(window, text=" ",bg="light blue",fg="black",font="none 15 bold" ).grid(row=1,column=0)
 enter_city=Entry(window, textvariable=search_city,bg="white",fg="black",font=("Comic Sans MS",16,"bold")).grid(row=2,column=0)
-Label(window, text=" ",bg="black",fg="white",font="none 15 bold" ).grid(row=3,column=0)
+Label(window, text=" ",bg="light blue",fg="black",font="none 15 bold" ).grid(row=3,column=0)
 
-search_button=Button(window,text='SEARCH WEATHER',command=get_weather_search,width=10,bg="brown",fg="black",font=("none",10,"bold")).grid(row=2,column=2)
-Label(window, text=" ",bg="black",fg="white",font="none 15 bold" ).grid(row=5,column=0)
+search_button=Button(window,text='SEARCH WEATHER',command=get_weather_search,width=0,bg="red",fg="black",font=("none",10,"bold")).grid(row=2,column=2)
+Label(window, text=" ",bg="light blue",fg="white",font="none 15 bold" ).grid(row=5,column=0)
 
-location=Label(window,textvariable=place,font=("\nComic Sans MS",30,"bold"),bg="brown").grid(row=6,column=0)
-Label(window, text="",bg="black",fg="white",font="none 15 bold" ).grid(row=7,column=0)
+location=Label(window,textvariable=place,font=("\nComic Sans MS",30,"bold"),bg="light blue").grid(row=6,column=0)
+Label(window, text="",bg="light blue",fg="white",font="none 15 bold" ).grid(row=7,column=0)
 
-temperature=Label(window,textvariable=temp,font=("\nComic Sans MS",30,"bold"),bg="brown").grid(row=10,column=0)
-Label(window, text=" ",bg="black",fg="white",font="none 15 bold" ).grid(row=9,column=0)
+temperature=Label(window,textvariable=temp,font=("\nComic Sans MS",30,"bold"),bg="light blue").grid(row=10,column=0)
+Label(window, text=" ",bg="light blue",fg="white",font="none 15 bold" ).grid(row=9,column=0)
 
-weather=Label(window,textvariable=temp_con,font=("\nclearComic Sans MS",30,"bold"),bg="brown").grid(row=12,column=0)
-Label(window, text=" ",bg="black",fg="white",font="none 15 bold" ).grid(row=11,column=0)
+weather=Label(window,textvariable=temp_con,font=("\nclearComic Sans MS",40,"bold"),bg="light blue").grid(row=12,column=0)
+Label(window, text=" ",bg="light blue",fg="white",font="none 15 bold" ).grid(row=11,column=0)
 
-dandt=Label(window,text='Thursday',font=("\nclearComic Sans MS",30,"bold"),bg="brown").grid(row=8,column=0)
-Label(window, text=" ",bg="black",fg="white",font="none 10 bold" ).grid(row=11,column=0)
-
-
-
-
-
+dandt=Label(window,text='Monday',font=("\nclearComic Sans MS",30,"bold"),bg="light blue").grid(row=7,column=0)
+Label(window, text=" ",bg="light blue",fg="white",font="none 10 bold" ).grid(row=11,column=0)
 window.mainloop()
